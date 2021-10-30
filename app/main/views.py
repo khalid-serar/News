4960c9a9b8e58295552e57c7252484e0b4f873d5
khalid-serar <khalid.serar@student.moringaschool.com>
2021-10-30 09:36:07 +0300

    add modified files

---
+++ app/main/views.py
@@ -0,0 +1,33 @@
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..request import get_news , get_article
+
+
+
+@main.route('/', methods=['GET'])
+def index():
+
+    '''
+    view news page function that returns the news details  and its data
+    '''
+    
+    news = get_news()
+    # print(news)
+    title = "News"
+    
+    return render_template('index.html', news = news, title=title)
+
+
+
+
+
+@main.route('/source/<id>')
+def article(id):
+    '''
+    view news page function that returns the news details  and its data
+    '''
+    
+    articles = get_article(id)
+    title = "News"
+    
+    return render_template('article.html', articles = articles, title=title)
